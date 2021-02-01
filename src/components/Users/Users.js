ad478935b9195876ef07fe380db858afc389576b
garet2gis <br-avm32@yandex.ru>
2021-02-01 16:29:03 +0300

    Users refactored as Class component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,46 +3,24 @@ import User from "./User/User";
 import * as axios from "axios";
 
 
-let Users = (props) =>{
-
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response =>{
-                props.setUsers(response.data.items)
-            })
-       /* props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://i.pinimg.com/736x/2d/0e/41/2d0e419c310033945063c6c9884b2725.jpg',
-                followed: true,
-                fullName: 'Vova',
-                status: 'going home',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 2, photoUrl: 'https://i.pinimg.com/564x/10/a1/37/10a137c1fba5460a3658e027135afc99.jpg',
-                followed: false,
-                fullName: 'Victor',
-                status: 'walking with dog',
-                location: {city: 'Saint Petersburg', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://assets.change.org/photos/0/ux/dp/DpuxDpYPEfMmBCI-800x450-noPad.jpg?1578666314',
-                followed: true,
-                fullName: 'Chungus',
-                status: 'big',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            }
-        ]);*/
+class Users extends React.Component {
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                })
+        }
     }
-    let users = props.users.map(u => <User key = {u.id} user = {u} follow = {props.follow} unfollow = {props.unfollow}/>);
-
-    return(
-        <div>
-            {users}
-        </div>
-    )
+    render() {
+        let users = this.props.users.map(u => <User key={u.id} user={u} follow={this.props.follow}
+                                                    unfollow={this.props.unfollow}/>);
+        return (
+            <div>
+                {users}
+            </div>
+        )
+    }
 }
 
 export default Users;
